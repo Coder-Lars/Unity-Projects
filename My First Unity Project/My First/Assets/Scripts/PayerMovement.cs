58d56d6e7972469090e33c99df554a59eed50442
Coder-Lars <71517515+Coder-Lars@users.noreply.github.com>
2020-11-01 17:57:28 +0100

    First Upload

---
+++ My First Unity Project/My First/Assets/Scripts/PayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D rb;
+    public Animator animator;
+    private bool facingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(moveBy));
+        Flip(moveBy);
+    }
+
+    private void Flip(float moveBy)
+    {
+        if (moveBy > 0 && !facingRight || moveBy < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+
+        }
+    }
+}
