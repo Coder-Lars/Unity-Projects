e2331ae104659224e7ad6ed4a983d96f540a9ffe
Coder-Lars <71517515+Coder-Lars@users.noreply.github.com>
2020-11-02 19:47:04 +0100

    Added Weopon

---
+++ 1st Person 3D Game/Assets/Scritps/Gun.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+   public float damage = 10f;
+   public float range = 100f;
+    public Animator animator;
+    public float fireRate = 100f;
+
+    public ParticleSystem muzzleFlash;
+
+    public Camera fpsCam;
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            animator.SetTrigger("Shoot");
+            Shoot();
+            
+        }
+    }
+
+    void Shoot()
+    {
+        
+        muzzleFlash.Play();
+        
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+
+            }
+
+            
+        }
+    }
+
+    
+}
--- 1st Person 3D Game/Assets/Scritps/PlayerMovement.cs
+++ 1st Person 3D Game/Assets/Scritps/PlayerMovement.cs
@@ -36,7 +36,9 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+       
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
