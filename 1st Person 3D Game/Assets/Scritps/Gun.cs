8ed1fe560a6b935c4a879bf2d1554054b61664a1
Coder-Lars <71517515+Coder-Lars@users.noreply.github.com>
2020-11-03 10:16:20 +0100

    n

--- 1st Person 3D Game/Assets/Scritps/Gun.cs
+++ 1st Person 3D Game/Assets/Scritps/Gun.cs
@@ -6,28 +6,47 @@ public class Gun : MonoBehaviour
    public float damage = 10f;
    public float range = 100f;
     public Animator animator;
+    public Animator animator2;
+    
     public float fireRate = 100f;
 
     public ParticleSystem muzzleFlash;
+    public GameObject ImpactHit;
+    
+    
 
     public Camera fpsCam;
     private float nextTimeToFire = 0f;
+
+
+    private void Start()
+    {
+       
+    }
+
+
     // Update is called once per frame
+
     void Update()
     {
             if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            
             nextTimeToFire = Time.time + 1f / fireRate;
             animator.SetTrigger("Shoot");
+            animator2.SetTrigger("isShot");
+            
             Shoot();
             
         }
+         
     }
 
     void Shoot()
     {
         
         muzzleFlash.Play();
+        FindObjectOfType<AudioManager>().Play("Laser13");
         
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
@@ -42,9 +61,14 @@ public class Gun : MonoBehaviour
 
             }
 
+            GameObject impactGO = Instantiate(ImpactHit, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
             
         }
     }
+
+   
+  
 
     
 }
